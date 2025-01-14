59fa5daffa57574759b3289e7d1ff7b63a194de9
ziegler <ziegler@edu-sharing.net>
2025-01-14 14:11:55 +0100

    feat: french translation

---
+++ lang/fr/assignsubmission_edusharing.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignsubmission_edusharing', language 'fr'
+ *
+ * @package    assignsubmission_edusharing
+ * @copyright  metaVentis GmbH — http://metaventis.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['countfiles'] = '{$a} fichiers';
+$string['default'] = 'activer par défaut ?';
+$string['default_help'] = 'Lorsqu\'elle est activée, la soumission edu-sharing est activée par défaut pour toutes les soumissions.';
+$string['description'] = '{$a} soumission';
+$string['descriptionofsubmission'] = 'Recherchez votre soumission dans edu-sharing...';
+$string['edu_extension_error'] = 'Veuillez ajouter l\'extension de fichier au nom du fichier.';
+$string['edu_filename'] = '{$a} nom de fichier';
+$string['edu_url'] = '{$a} URL';
+$string['edusharing'] = '{$a}';
+$string['enabled'] = 'soumissions edu-sharing';
+$string['enabled_help'] = 'Lorsqu\'ils sont activés, les apprenants peuvent télécharger un ou plusieurs fichiers depuis edu-sharing pour leur soumission.';
+$string['eventassessableuploaded'] = 'Un fichier {$a} a été soumis';
+$string['maxfiles'] = 'Nombre maximum de fichiers par soumission';
+$string['maxfiles_help'] = 'Si {$a} soumissions sont activées, c\'est le nombre de fichiers qui peuvent être acceptés par soumission.';
+$string['maxfilessubmission'] = '{$a}: Nombre maximum de fichiers';
+$string['maxfilessubmission_help'] = 'Lorsque {$a} soumissions sont activées, les apprenants peuvent télécharger autant de fichiers par soumission.';
+$string['pluginname'] = 'soumissions edu-sharing';
+$string['remove_es_object'] = 'supprimer l\'objet edu-sharing';
+$string['searchrec'] = 'Rechercher dans le référentiel {$a} ...';
+$string['uploadrec'] = 'Rechercher dans le référentiel {$a} ...';
